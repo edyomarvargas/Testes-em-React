049fdfcb0262968b4a32143dfae82eda4339489e
Edyomar Vargas <edyomar-vargas@hotmail.com>
2022-05-09 19:45:17 -0300

    Adiciona requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import React from 'react';
+import { NotFound } from '../components';
+import renderWithRouter from '../RenderWithRouter';
+
+test('Testa se a página contém um heading com o texto "Page requested not found"', () => {
+  renderWithRouter(<NotFound />);
+
+  const textInPage = /page requested not found/i;
+  const title = screen.getByRole('heading', { name: textInPage, level: 2 });
+  expect(title).toBeInTheDocument();
+});
+
+test('Testa se a página renderiza a imagem correta', () => {
+  renderWithRouter(<NotFound />);
+
+  const image = screen.getByRole('img', { name: /pikachu crying because the page/i });
+  expect(image).toBeInTheDocument();
+  expect(image.src).toContain('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
